b475bba5d1e1806f9ba1f6c6c4daa97a59cd6c6d
OfalWoz <lsk.craft@gmail.com>
2021-10-23 18:26:34 +0200

    Final version of counting app

--- app/src/main/java/pl/ofalwoz/counter/MainActivity.java
+++ app/src/main/java/pl/ofalwoz/counter/MainActivity.java
@@ -3,12 +3,40 @@ package pl.ofalwoz.counter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int count;
+    private TextView showCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showCount = findViewById(R.id.show_count);
+    }
+
+    public void CountUpButton(View view) {
+        count++;
+        if (showCount != null){
+            showCount.setText(Integer.toString(count));
+        }
+    }
+
+    public void CountDownButton(View view) {
+        count--;
+        if (showCount != null){
+            showCount.setText(Integer.toString(count));
+        }
+    }
+
+    public void ResetButton(View view) {
+        count = 0;
+        if (showCount != null){
+            showCount.setText(Integer.toString(count));
+        }
     }
 }
